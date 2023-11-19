15b73a8dd59c5fe562f6e51277c8df5dd7cf20e1
Pablo Reyes <pabloandresreyes2004@gmail.com>
2023-11-19 13:57:08 -0500

    Modelos

---
+++ app/Models/Estudiante.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Estudiante extends Model
+{
+    protected $fillable = [
+        'nombre',
+        'apellido',
+        'email',
+    ];
+    use HasFactory;
+
+    public function inscripciones()
+    {
+        return $this->hasMany(Inscripcion::class);
+    }
+}
